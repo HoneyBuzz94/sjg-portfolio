d282e490e92c36ddba6489bbfe28d1a8c1964722
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-03-18 12:02:12 -0600

    Reformatted contact page

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,22 +1,26 @@
 import { useState } from "react";
 
 export default function Nav({ links }) {
-
   const [isActive, setIsActive] = useState(false);
 
   const handleActive = () => {
-    setIsActive(!isActive); // Toggle the active state
+    setIsActive(!isActive);
   };
 
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation" style={{backgroundColor: '#F0DCCA'}}>
+    <nav
+      className="navbar"
+      role="navigation"
+      aria-label="main navigation"
+      style={{ backgroundColor: "#F0DCCA" }}
+    >
       <div className="navbar-brand">
         <a href="/">
           <img src="../../../images\HoneyBuzz94 Logo (small).png" width="100" />
         </a>
         <a
           role="button"
-          className={`navbar-burger ${isActive ? 'is-active' : ''}`}
+          className={`navbar-burger ${isActive ? "is-active" : ""}`}
           aria-label="menu"
           aria-expanded="false"
           data-target="main-navbar"
@@ -27,7 +31,10 @@ export default function Nav({ links }) {
           <span aria-hidden="true"></span>
         </a>
       </div>
-      <div id="main-navbar" className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
+      <div
+        id="main-navbar"
+        className={`navbar-menu ${isActive ? "is-active" : ""}`}
+      >
         <h1 className="navbar-item is-size-1 has-text-weight-bold is-hidden-touch">
           Sam Grow
         </h1>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,40 +1,126 @@
+import { useState } from "react";
+import validateEmail from "../../utils/helpers";
+
+const styles = {
+  tile: {
+    backgroundColor: "#F0DCCA",
+  },
+};
+
 export default function Contact() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState("is-hidden");
+
+  const handleInputChange = (e) => {
+    setSuccess("is-hidden");
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "name") {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    
+    setSuccess('');
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
   return (
-    <div className="tile is-ancestor is-6 m-0">
-      <div className="box has-background-warning tile is-parent is-vertical">
-        <div className="tile is-parent">
-          <div className="tile is-child">
-            <label className="label">Name</label>
-            <div className="control">
-              <input className="input" type="text" placeholder="Text" />
+    <div className="tile is-ancestor is-9 m-0">
+      <div className="tile is-vertical">
+        <form
+          className="tile is-parent is-vertical box form"
+          onSubmit={handleFormSubmit}
+          style={styles.tile}
+        >
+          <div className="tile is-parent">
+            <div className="tile is-child">
+              <label className="label">Name</label>
+              <div className="control">
+                <input
+                  className="input"
+                  name="name"
+                  type="text"
+                  placeholder="Name"
+                  onChange={handleInputChange}
+                  value={name}
+                  required
+                />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="tile is-parent">
-          <div className="tile is-child">
-            <label className="label">Email</label>
-            <div className="control">
-              <input className="input" type="email" placeholder="Email" />
+          <div className="tile is-parent">
+            <div className="tile is-child">
+              <label className="label">Email</label>
+              <div className="control">
+                <input
+                  className={`input ${email!='' && !validateEmail(email) ? 'is-danger' : ''}`}
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  onChange={handleInputChange}
+                  value={email}
+                  required
+                />
+                <p class={`help is-danger ${email!='' && !validateEmail(email) ? '' : 'is-hidden'}`}>This email is invalid</p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="tile is-parent">
-          <div className="tile is-child">
-            <label className="label">Message</label>
-            <div className="control">
-              <textarea className="textarea" placeholder="Textarea"></textarea>
+          <div className="tile is-parent">
+            <div className="tile is-child">
+              <label className="label">Message</label>
+              <div className="control">
+                <textarea
+                  className="textarea"
+                  name="message"
+                  type="textarea"
+                  placeholder="Textarea"
+                  onChange={handleInputChange}
+                  value={message}
+                  required
+                ></textarea>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="tile is-parent">
-          <div className="tile is-child">
-            <div className="control">
-              <button className="button is-link">Submit</button>
+          <div className="tile is-parent">
+            <div className="tile is-child">
+              <div className="control">
+                <button className={`button is-link ${success ? '' : 'is-hidden'}`}>Submit</button>
+              </div>
+              <p className={`has-text-success has-text-weight-bold ${success}`}>Your message was successfully submitted!</p>
             </div>
           </div>
+        </form>
+        <div
+          className="tile is-parent is-vertical box has-text-centered"
+          style={styles.tile}
+        >
+          <p>
+            You can also contact me at:{" "}
+            <a href="mailto:saybejay@gmail.com">saybejay@gmail.com</a>
+          </p>
+          <p>
+            Or message me on <a href="https://github.com/HoneyBuzz94">GitHub</a>{" "}
+            or{" "}
+            <a href="https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Afsd_profile%3AACoAACPmBlgB-xYQKaSlpt5a7pgDv3kBm_9GhU4&keywords=samuel%20grow&origin=RICH_QUERY_SUGGESTION&position=0&searchId=df09f6e9-17d5-4e1a-bd35-76237d168ac9&sid=X6E&spellCorrectionEnabled=false">
+              Linkedin
+            </a>
+          </p>
         </div>
       </div>
     </div>
