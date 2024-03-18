101419cb6398a0ce2a1cd3df9cf28e209eddc301
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-03-18 11:05:18 -0600

    Made mobile navbar functional

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 export default function Nav({ links }) {
+
+  const [isActive, setIsActive] = useState(false);
+
+  const handleActive = () => {
+    setIsActive(!isActive); // Toggle the active state
+  };
+
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation" style={{backgroundColor: '#F0DCCA'}}>
       <div className="navbar-brand">
@@ -7,17 +16,18 @@ export default function Nav({ links }) {
         </a>
         <a
           role="button"
-          className="navbar-burger"
+          className={`navbar-burger ${isActive ? 'is-active' : ''}`}
           aria-label="menu"
           aria-expanded="false"
           data-target="main-navbar"
+          onClick={handleActive}
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
-      <div id="main-navbar" className="navbar-menu">
+      <div id="main-navbar" className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
         <h1 className="navbar-item is-size-1 has-text-weight-bold is-hidden-touch">
           Sam Grow
         </h1>
